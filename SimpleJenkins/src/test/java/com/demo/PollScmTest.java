86db3e869ad22670c3d79ee51bb8a10651286537
Bhimrao <bhimraokulkarni123@gmail.com>
2025-02-06 13:54:08 +0530

    commit

---
+++ SimpleJenkins/src/test/java/com/demo/PollScmTest.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollScmTest {
+@Test
+public void test()
+{
+	Reporter.log("PollScm test added",true);
+}
+}
